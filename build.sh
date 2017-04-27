72efabb353b49ce176edb768f59fbcd645f0e8b3
Tomas Roos <ptomasroos@gmail.com>
2017-04-27 11:45:43 +0200

    Added build procedure

---
+++ build.sh
@@ -0,0 +1,4 @@
+rm -fr node_modules
+npm install
+rm -fr build/dist/prebid.*
+gulp build --tag `date +"d%Y%m%d%s"` --adapters customAdaptersDesktop.json --analyticsAdapters customAnalyticsAdapters.json
