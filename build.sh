558a1562953ee2108fe0f58c7c566205f5885264
Tomas Roos <ptomasroos@gmail.com>
2017-08-21 20:22:10 +0200

    Small fix

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 rm -fr build/dist/
 gulp build --modules=modules.json
-mv build/dist/prebid.js build/dist/prebid.d`date +"d%Y%m%d%s"`.js
+mv build/dist/prebid.js build/dist/prebid.`date +"d%Y%m%d%s"`.js
