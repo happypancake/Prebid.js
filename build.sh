8c1cf0e8e46067ae5bc8bca4c6dba7bca375f76a
Tomas Roos <ptomasroos@gmail.com>
2018-05-17 20:29:36 +0200

    new syntax.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -fr build/dist/
 rm -fr node_modules
 yarn install
 gulp build --modules=modules.json
-mv build/dist/prebid.js build/dist/prebid.`date "d%Y%m%d%s"`.js
+mv build/dist/prebid.js build/dist/prebid.`date +"d%Y%m%d%s"`.js
 open build/dist/
