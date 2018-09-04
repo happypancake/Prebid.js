71b41d25d8dc6ecf52066a51405205c41a4df2be
Tomas Roos <ptomasroos@gmail.com>
2018-09-04 08:51:44 +0200

    Make build use npm instead

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm -fr build/dist/
 rm -fr node_modules
-yarn install
+npm install
 gulp build --modules=modules.json
 mv build/dist/prebid.js build/dist/prebid.`date +"d%Y%m%d%s"`.js
 open build/dist/
